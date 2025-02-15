f7b2d6f73d779f0e9a6c8461380edba1777a41c6
artym <artym.surkov@mail.ru>
2025-02-16 03:38:28 +0500

    init

---
+++ src/main/java/org/surkov/hranalyzer/HrAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package org.surkov.hranalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/surkov/hranalyzer/HrAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.surkov.hranalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
